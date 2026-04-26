4b19fdf8e4db559df618ae402d71bef6e38e157f
eusants01 <daninilbr978@gmail.com>
2026-04-26 03:35:08 -0300

    correção geral 2.4

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import discord
 import asyncio
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 intents = discord.Intents.default()
 intents.guilds = True
@@ -9,17 +9,41 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+status_list = [
+    "🩸 Em expansão de domínio...",
+    "🔮 Julgando maldições...",
+    "⚔️ Apenas os fortes sobrevivem",
+    "👁️ Observando os fracos...",
+    "🔥 Despertando energia amaldiçoada",
+    "💀 Você está dentro do domínio...",
+]
+
+@tasks.loop(seconds=15)
+async def trocar_status():
+    for status in status_list:
+        atividade = discord.Game(name=status)
+        await bot.change_presence(
+            status=discord.Status.online,
+            activity=atividade
+        )
+        await asyncio.sleep(15)
+
 @bot.event
 async def on_ready():
     print(f"✅ Bot online como {bot.user}")
+
+    if not trocar_status.is_running():
+        trocar_status.start()
 
 async def main():
     async with bot:
         print("Iniciando bot...")
         await bot.load_extension("cogs.tickets")
-        print("Cog tickets carregada.")
+
         token = os.getenv("DISCORD_TOKEN")
+        print("DISCORD_TOKEN:", token)
         print("Token encontrado:", bool(token))
+
         await bot.start(token)
 
 asyncio.run(main())
