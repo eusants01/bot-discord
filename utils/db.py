1281c5ce79f9fe3e683bf4a1dc8494a00c5408fc
eusants01 <daninilbr978@gmail.com>
2026-05-01 20:46:50 -0300

    correção simples

--- utils/db.py
+++ utils/db.py
@@ -1,36 +1,37 @@
-import sqlite3
+import os
+import psycopg2
 
-conn = sqlite3.connect("conquistas.db")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL não encontrada no Railway.")
+
+conn = psycopg2.connect(DATABASE_URL)
 cursor = conn.cursor()
 
 def criar_tabelas():
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS conquistas (
-        user_id TEXT,
-        conquista_id TEXT,
-        data TEXT
-    )
+        CREATE TABLE IF NOT EXISTS conquistas (
+            user_id TEXT,
+            conquista_id TEXT,
+            data TEXT,
+            PRIMARY KEY (user_id, conquista_id)
+        )
     """)
 
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS mensagens (
-        user_id TEXT PRIMARY KEY,
-        total INTEGER
-    )
+        CREATE TABLE IF NOT EXISTS mensagens (
+            user_id TEXT PRIMARY KEY,
+            total INTEGER DEFAULT 0
+        )
     """)
 
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS codigos (
-        codigo TEXT,
-        user_id TEXT
-    )
-    """)
-
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS titulos (
-        user_id TEXT PRIMARY KEY,
-        titulo TEXT
-    )
+        CREATE TABLE IF NOT EXISTS codigos (
+            codigo TEXT,
+            user_id TEXT,
+            PRIMARY KEY (codigo, user_id)
+        )
     """)
 
     conn.commit()
